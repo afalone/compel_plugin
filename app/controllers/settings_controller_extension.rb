e48be53e8b15877a05cb2a9ffca73f5c6696c9c8
afa <afa@afalone.local>
2011-07-12 13:55:57 +0400

    fix order

--- app/controllers/settings_controller_extension.rb
+++ app/controllers/settings_controller_extension.rb
@@ -9,16 +9,10 @@ module SettingsControllerExtension
    module InstanceMethods
      def plugin_with_compel_queries
         plugin_without_compel_queries
-        puts "==== fix cntrlr"
        if params["id"] == "compel_plugin"
-        puts "==== fix cntrlr saving"
-        p params["queries"]
         if params["queries"]
-         puts "==== update"
          Query.update params["queries"].keys, params["queries"].values
         end
-       else
-        puts "==== fix cntrlr nonneeded"
        end
      end
    end
